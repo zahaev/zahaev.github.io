bebb1837251414ee0ff175ae1cad005582167b6f
zahaev <113417779+zahaev@users.noreply.github.com>
2022-12-12 22:26:48 +0300

    Update add.js

--- 6/add.js
+++ 6/add.js
@@ -1,19 +1,20 @@
 <!---чек убирается а кнопка нет --->
-function umnoz() {
+function umnoz(){
     let stoim = document.getElementById("stoim").value;
     let colvo = document.getElementById("colvo").value;
     let rezult = document.getElementById("rezult");
 
-    if (stoim == "" || colvo == "")
-        rezult.innerHTML = "Âû íå ââåëè ñòîèìîñòü èëè êîëè÷åñâî òîâàðà"
-    else if (stoim != parseInt(stoim) || colvo != parseInt(colvo) || parseInt(colvo) < 0 || parseInt(stoim) < 0)
-        rezult.innerHTML = "Îäíî èç ïîëåé ñîäåðæèò íåäîïóñòèìîå óñëîâèå äëÿ çàäà÷è èëè íåäîïóñòèìûå ñèìâîëû";
+    if(stoim ==="" || colvo ==="")
+        rezult.innerHTML = "не введена стоимость/количество товара"
+    else if(stoim != parseInt(stoim) || colvo != parseInt(colvo) || parseInt(colvo)<0 || parseInt(stoim)<0)
+        rezult.innerHTML = "Одно из полей содержит недопустимое условие для задачи или недопустимые символы";
     else
-        rezult.innerHTML = "Ðåçóëüòàò: " + parseInt(colvo) * parseInt(stoim);
+        rezult.innerHTML = "Результат: " + parseInt(colvo)*parseInt(stoim); 
 }
 
-window.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("but").addEventListener("click", umnoz);
+window.addEventListener("DOMContentLoaded", function() {
+    let buttom = document.getElementById("but");
+    buttom.addEventListener("click", umnoz);
 });
 
 window.addEventListener("DOMContentLoaded", function () {
